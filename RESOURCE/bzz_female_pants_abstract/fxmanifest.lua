d4427b512a8e741f5d4d3b97eeed7a635ee64637
Guudz <Kilian1702@outlook.com>
2023-02-07 03:07:47 +0100

    Initial commit

---
+++ RESOURCE/bzz_female_pants_abstract/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'bodacious'
+game { 'gta5' }
+author 'BzZz'
+description 'Addon clothes - Female abstract'
+
+files {
+  'mp_f_freemode_01_mp_f_bz_f_abstract_oi.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_bz_f_abstract_oi.meta'
